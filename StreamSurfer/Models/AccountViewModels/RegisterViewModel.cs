da7da9b1ea0a4902d7705efb9831290d7dfe2cd1
CamD67 <cameron.doane@gmail.com>
2017-05-10 13:24:55 -0700

    add confirmation password, close #18

--- StreamSurfer/Models/AccountViewModels/RegisterViewModel.cs
+++ StreamSurfer/Models/AccountViewModels/RegisterViewModel.cs
@@ -17,6 +17,11 @@ namespace StreamSurfer.Models.AccountViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [EmailAddress]
+        [Display(Name = "Confirm Email")]
+        [Compare("Email", ErrorMessage ="The email and confirmation email do not match")]
+        public string ConfirmEmail { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
