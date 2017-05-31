26c4b40aa132eaf81f1da6db4d4f762cf1d4322c
Jack Whitesell <jackw117@uw.edu>
2017-05-31 14:47:27 -0700

    small bug fixes

--- StreamSurfer/wwwroot/js/browse.js
+++ StreamSurfer/wwwroot/js/browse.js
@@ -1,1 +1,6 @@
-﻿
+﻿$(document).ready(function () {
+    $(".right-button").click(function () {
+        var scrollLeft = $(this).parent().scrollLeft();
+        $("#browseScrollDiv").animate({ scrollLeft: scrollLeft + 200 }, 300);
+    });
+});
